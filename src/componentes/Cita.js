79253b42d8d9be52b158e7e5aaacd56a1929beed
NicoPaladini <elpala2009.np@gmail.com>
2018-12-12 02:08:24 -0300

    Added LocalStorage Functions and PropTypes

--- src/componentes/Cita.js
+++ src/componentes/Cita.js
@@ -3,13 +3,13 @@ import React, { Component } from 'react'
 class Cita extends Component{
 
     eliminarCita = () =>{
-        
+
         this.props.borrarCita( this.props.info.id );
     }
 
     render(){
 
-        const {fecha, hora, mascota, propietario, sintomas, id}= this.props.info;
+        const {fecha, hora, mascota, propietario, sintomas}= this.props.info;
 
         return(
             <div className="media mt-3">
